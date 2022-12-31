5d1a92ed5a175000a92b5599b3b00d4dd58d994a
PythonicWarrior <97762514+PythonicWarrior@users.noreply.github.com>
2022-12-31 14:20:47 -0600

    Begin 6502 asm instruction modeling - Defined ~%10 of 6502 opcodes - Started initialization of opcodes - Changed status from uint8_t to bool array

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -2,10 +2,21 @@
 
 namespace NESBox
 {
+	uint8_t CPU::Instructions::ADCImmediate(CPU* cpu)
+	// 2 bytes, 2 cycles
+	{
+		cpu->accumulator += cpu->ReadAddress(++pgrm_counter);
+		return 2;
+	}
+
+
     uint8_t CPU::Instruction(uint8_t instruction){
         switch(instruction){
-			//TODO: convert to LittleEndian
+			/*
+				Storage Instructions
+			*/
             case 0xA9: //LDA Immediate (2 bytes, 2 cycles)
+				cycles += 2
 				accumulator = hardware_bus->read_address(++pgrm_counter);	// 1
 				status[7] = accumulator < 0x80;
 				status[1] = accumulator == 0x00;
@@ -29,8 +40,8 @@ namespace NESBox
 				break;
 			case 0xAD: //LDA Absolute (3 bytes, 4 cycles)
 				uint16_t readingAddress =
-					(hardware_bus->read_address(++pgrm_counter) << 8) +		// 1
-					hardware_bus->read_address(++pgrm_counter);				// 2
+					hardware_bus->read_address(++pgrm_counter) +			// 1
+					(hardware_bus->read_address(++pgrm_counter) << 8);		// 2
 				accumulator = hardware_bus->read_address(readingAddress);	// 3
 				status[7] = accumulator < 0x80;
 				status[1] = accumulator == 0x00;
@@ -38,8 +49,8 @@ namespace NESBox
 				break;
 			case 0xBD: //LDA Absolute, X (3 bytes, 4 or 5 cycles)
 				uint16_t readingAddress =
-					(hardware_bus->read_address(++pgrm_counter) << 8) +		// 1,(3)
-					hardware_bus->read_address(++pgrm_counter) + x;			// 2
+					hardware_bus->read_address(++pgrm_counter) + x +		// 1,(2)
+					(hardware_bus->read_address(++pgrm_counter) << 8);		// 3
 				accumulator = hardware_bus->read_address(readingAddress);	// 4
 				status[7] = accumulator < 0x80;
 				status[1] = accumulator == 0x00;
@@ -47,8 +58,8 @@ namespace NESBox
 				break;
 			case 0xB9: //LDA Absolute, Y (3 bytes, 4 or 5 cycles)
 				uint16_t readingAddress =
-					(hardware_bus->read_address(++pgrm_counter) << 8) +		// 1,(3)
-					hardware_bus->read_address(++pgrm_counter) + y;			// 2
+					hardware_bus->read_address(++pgrm_counter) + y +		// 1,(2)
+					(hardware_bus->read_address(++pgrm_counter) << 8);		// 3
 				accumulator = hardware_bus->read_address(readingAddress);	// 4
 				status[7] = accumulator < 0x80;
 				status[1] = accumulator == 0x00;
@@ -58,19 +69,19 @@ namespace NESBox
 				uint16_t where = hardware_bus->read_address(				// 2
 					(++pgrm_counter) + x);									// 1
 				uint16_t readingAddress =
-					(hardware_bus->read_address(where) << 8) +				// 3
-					(hardware_bus->read_address(++where));					// 4
+					(hardware_bus->read_address(where)) +					// 4
+					(hardware_bus->read_address(++where) << 8);				// 3
 				accumulator = hardware_bus->read_address(readingAddress);	// 5
 				status[7] = accumulator < 0x80;
 				status[1] = accumulator == 0x00;
 				pgrm_counter++; 											// 6
 				break;
 			case 0xB1: // LDA Indirect, Y (2 bytes, 5 or 6 cycles)
-				uint16_t where = hardware_bus->read_address(				// 2
+				uint16_t where = hardware_bus->read_address(
 					(++pgrm_counter));						 				// 1
 				uint16_t readingAddress =
-					(hardware_bus->read_address(where) << 8) +			  	// 3
-					(hardware_bus->read_address(++where)) + y;			  	// 4
+					(hardware_bus->read_address(++where)) + y +			  	// 2,(3)
+					(hardware_bus->read_address(where) << 8);			  	// 4
 				accumulator = hardware_bus->read_address(readingAddress); 	// 5
 				status[7] = accumulator < 0x80;
 				status[1] = accumulator == 0x00;
@@ -96,6 +107,29 @@ namespace NESBox
 				status[7] = x&BITSELECT[7];
 				status[1] = x == 0x00;
 				pgrm_counter++;												// 4
+				break;
+			case 0xAE: // LDX Absolute (3 bytes, 4 cycles)
+				uint16_t readingAddress =
+					hardware_bus->read_address(++pgrm_counter) +			// 1
+					(hardware_bus->read_address(++pgrm_counter) << 8);		// 2
+				x = hardware_bus->read_address(readingAddress);				// 3
+				status[7] = x & BITSELECT[7];
+				status[1] = x == 0x00;
+				pgrm_counter++;												// 4
+				break;
+			case 0xBE:
+
+				/*
+					Math Instructions
+				*/
+
+			case 0x69: // ADC Immediate (2 bytes, 2 cycles)					
+				accumulator += instruction & 0x00FF;						// 1,2
+				break;
+			case 0x65: // ADC Zero-Page (2 bytes, 3 cycles)
+				accumulator += hardware_bus->read_address(					// 1, 2, 3
+										instruction & 0x00FF
+										);	
 				break;
 		}
     };
--- src/include/CPU.hpp
+++ src/include/CPU.hpp
@@ -2,21 +2,28 @@
 #ifndef _CPU_H
 #define _CPU_H
 
+/*
+    OPCODE():
+        #do operation
+        delay(delta - ((double)operationCycleTotal
+            / 1000000)
+*/
 
 namespace NESBox
 {
     // Bit Selection List
     const uint8_t BITSELECT[]
     {
-        0xFF-0x01,
-        0xFF-0x02,
-        0xFF-0x04,
-        0xFF-0x08,
-        0xFF-0x10,
-        0xFF-0x20,
-        0xFF-0x40,
-        0xFF-0x80
+        0x01,
+        0x02,
+        0x04,
+        0x08,
+        0x10,
+        0x20,
+        0x40,
+        0x80
     };
+
 
     class CPU : public Hardware
     {
@@ -33,12 +40,72 @@ namespace NESBox
         uint8_t y;
         uint8_t stack_ptr;
 
-        // Program counter
+        // Program Counter
         uint16_t pgrm_counter;
 
-        // Status register
-        uint8_t status;
+        // Status Register
+        bool status[8];
+        
+        /* 
+            Emulation Data
+        */
+        
+        // Cycle Counter
+        uint8_t cycles;
 
+    protected:
+        class Instructions
+        {
+            /* These instructions return
+                the number of cycles performed. */
+
+            /*
+                Storage Instructions
+            */
+            // LDA =========================
+            uint8_t LDAImmediate (CPU* cpu);
+            uint8_t LDAZeroPage  (CPU* cpu);
+            uint8_t LDAZeroPageX (CPU* cpu);
+            uint8_t LDAAbsolute  (CPU* cpu);
+            uint8_t LDAAbsoluteX (CPU* cpu);
+            uint8_t LDAAbsoluteY (CPU* cpu);
+            uint8_t LDAIndirectX (CPU* cpu);
+            uint8_t LDAIndirectY (CPU* cpu);
+            // LDX =========================
+            uint8_t LDXImmediate (CPU* cpu);
+            uint8_t LDXZeroPage  (CPU* cpu);
+            uint8_t LDXZeroPageX (CPU* cpu);
+            uint8_t LDXAbsolute  (CPU* cpu);
+            uint8_t LDXAbsoluteX (CPU* cpu);
+            // LDY =========================
+            uint8_t LDYImmediate (CPU* cpu);
+            uint8_t LDYZeroPage  (CPU* cpu);
+            uint8_t LDYZeroPageX (CPU* cpu);
+            uint8_t LDYAbsolute  (CPU* cpu);
+            uint8_t LDYAbsoluteX (CPU* cpu);
+            // STA =========================
+            uint8_t STAZeroPage  (CPU* cpu);
+            uint8_t STAZeroPageX (CPU* cpu);
+            uint8_t STAAbsolute  (CPU* cpu);
+            uint8_t STAAbsoluteX (CPU* cpu);
+            uint8_t STAAbsoluteY (CPU* cpu);
+            uint8_t STAIndirextX (CPU* cpu);
+            uint8_t STAIndirectY (CPU* cpu);
+
+            /*
+                Math Instructions
+            */
+            // ADC ==========================
+            uint8_t ADCImmediate (CPU* cpu);
+            uint8_t ADCZeroPage  (CPU* cpu);
+            uint8_t ADCZeroPageX (CPU* cpu);
+            uint8_t ADCZeroPageY (CPU* cpu);
+            uint8_t ADCAbsolute  (CPU* cpu);
+            uint8_t ADCAbsoluteX (CPU* cpu);
+            uint8_t ADCAbsoluteY (CPU* cpu);
+            uint8_t ADCIndirectX (CPU* cpu);
+            uint8_t ADCIndirectY (CPU* cpu);
+        };
 
     public:
         CPU();
